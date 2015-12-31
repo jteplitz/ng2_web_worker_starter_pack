f07d301c3be686fb8c3386ef7ce6c72d8fe8bda0
Jason Teplitz <jteplitz602@gmail.com>
2015-12-30 19:12:05 -0500

    feat(angular): Upgrade to angular beta 0

--- src/app/background.ts
+++ src/app/background.ts
@@ -1,4 +1,10 @@
-import {bootstrapWebWorker} from "angular2/web_worker/worker";
+import {platform} from "angular2/core";
+import {
+  WORKER_APP_PLATFORM,
+  WORKER_APP_APPLICATION
+} from "angular2/platform/worker_app";
 import {MyApp} from "./component";
 
-bootstrapWebWorker(MyApp);
+platform([WORKER_APP_PLATFORM])
+.application([WORKER_APP_APPLICATION])
+.bootstrap(MyApp);
--- src/loader.js
+++ src/loader.js
@@ -1,5 +1,6 @@
 importScripts("../node_modules/systemjs/dist/system.src.js",
-              "../node_modules/angular2/bundles/web_worker/worker.dev.js");
+              "../node_modules/angular2/bundles/web_worker/worker.dev.js",
+              "../node_modules/angular2/bundles/angular2-polyfills.js");
 System.config({
     packages: {"app": {defaultExtension: "js"}}
 });
